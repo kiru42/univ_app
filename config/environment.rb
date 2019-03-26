cfb1487baf703162c6c9b64ada6a9eaff7e0e925
Kiruban PREMKUMAR <kiru42@gmail.com>
2019-03-26 15:38:25 +0100

    Added flash messages, forms error message, fadeOut using jquery & removed the default error tag added to forms' input

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Remove broken inputs by field_with_errors
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+  html_tag
+end
