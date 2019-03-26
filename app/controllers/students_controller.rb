0e03225be50656e4f3c4821de79588775104d7d8
Kiruban PREMKUMAR <kiru42@gmail.com>
2019-03-26 16:19:01 +0100

    Added Edit form & edit action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,10 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
   def new
     @student = Student.new
   end
