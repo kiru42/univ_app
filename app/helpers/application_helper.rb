eacb19c4025c1580b2d7b0c71630ef346adcc0da
Kiruban PREMKUMAR <kiru42@gmail.com>
2019-03-26 20:28:51 +0100

    Create and destroy sessions - add auth methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def session_link
+    if(logged_in?)
+      link_to("logout", logout_path, method: :delete)
+    else
+      link_to("log in", login_path)
+    end
+  end
 end
